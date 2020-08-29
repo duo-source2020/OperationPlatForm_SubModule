6560fc20ca1c6ca8e0274822230536377027f9b9
Christina.huang <Christina.huang@halliburton.com>
2020-08-29 12:19:04 -0500

    Add files

---
+++ Operation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationPlatForm
+{
+	public class Operation
+	{
+		//declare delegate
+		public delegate int IntegerOperation(int par1, int par2);
+
+		public static int Run(int b1, int b2, IntegerOperation mathOperation)
+		{
+			Predicate<int> isOk = GreaterThan;
+
+			Console.WriteLine(isOk(b1));
+
+			return mathOperation(b1, b2);
+		}
+
+		public static bool GreaterThan(int x)
+		{
+			return x> 0; 
+		}
+	}
+}
